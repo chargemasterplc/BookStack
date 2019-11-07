5310b7b7886a43f06a527a7acead6d57d6e06bfc
Aaron Noble <aaron.noble@chargemasterplc.com>
2019-11-07 10:16:24 +0000

    New DB config

--- app/Config/database.php
+++ app/Config/database.php
@@ -61,17 +61,24 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host'      => $mysql_host,
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'port'      => $mysql_port,
+            'read'      => [
+                'host'      => env('DB_ROUTER_HOST', '0.0.0.0'),
+                'port'      => env('DB_READ_PORT', '6447'),
+            ],
+            'write'     => [
+                'host'      => env('DB_ROUTER_HOST', '0.0.0.0'),
+                'port'      => env('DB_WRITE_PORT', '6446')
+            ],
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix'    => '',
             'prefix_indexes' => true,
             'strict'    => false,
+            'sticky'    => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
